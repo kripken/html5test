f124543687844e38536445f848050a303c00aea4
Alon Zakai <alonzakai@gmail.com>
2013-03-23 21:08:06 -0700

    formatting

--- scripts/data.js
+++ scripts/data.js
@@ -2,20 +2,21 @@ var tests = [
   {
     id:    'ES6',
     name:  'ECMAScript 6 (ES6)',
-    items:  [{
-      id:    'spread array',
-      name:   'Spread array (...) operator', 
-      url:  ''
-    }, {
-      id:    'spread call',
-      name:   'Spread call (...) operator',
-      url:    ''
-    }, {
-      id:    'arrow functions',
-      name:   'Short arrow function syntax ( => )',
-      url:    'http://wiki.ecmascript.org/doku.php?id=harmony:arrow_function_syntax'
-    }, 
-    '<em>Notice.</em>',
+    items:  [
+      {
+        id:    'spread array',
+        name:   'Spread array (...) operator', 
+        url:  ''
+      }, {
+        id:    'spread call',
+        name:   'Spread call (...) operator',
+        url:    ''
+      }, {
+        id:    'arrow functions',
+        name:   'Short arrow function syntax ( => )',
+        url:    'http://wiki.ecmascript.org/doku.php?id=harmony:arrow_function_syntax'
+      }, 
+      '<em>Notice.</em>',
     ],
   },
   'Experimental',
